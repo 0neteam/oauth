f9910476e9e6419c26502781af5d81fdb79e7473
ljh5432@gmail.com <ljh5432@gmail.com>
2025-03-25 20:26:18 +0900

    오류 수정

---
+++ oauth2/oauth2/src/main/java/com/java/oauth2/common/Utils.java
@@ -0,0 +1,59 @@
+package com.java.oauth2.common;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class Utils {
+
+    private final JwtDecoder jwtDecoder;
+
+    private final JWKSet jwkSet;
+
+    public String getUserNo (HttpServletRequest request) {
+
+        String userNo = "";
+
+        //쿠키 값 확인
+        Cookie[] cookies = request.getCookies();
+        //
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("access_token".equals(cookie.getName())) {
+                    List<JWK> jwks = jwkSet.getKeys();
+                    String token = cookie.getValue();
+                    System.out.println("token = " + token);
+
+                    try{
+
+                        // JwtDecoder를 사용하여 토큰 디코딩
+                        Jwt jwt = jwtDecoder.decode(token);
+                        // 🔹 디버깅 로그 출력 (토큰 클레임 및 만료 시간)
+                        System.out.println("Cafe Decoded JWT claims: " + jwt.getClaims());
+                        userNo = (String) jwt.getClaims().get("userNo");
+                        System.out.println("Cafe controller userNo : " + userNo);
+
+
+                    } catch (JwtException e) {
+                        // 토큰 처리 중 오류가 발생한 경우 로그아웃처리
+                        return "invaildToken";
+                    }
+
+                }
+            }
+        }
+
+        return userNo;
+    }
+
+}
--- oauth2/oauth2/src/main/java/com/java/oauth2/controller/OAuthClientController.java
+++ oauth2/oauth2/src/main/java/com/java/oauth2/controller/OAuthClientController.java
@@ -1,8 +1,14 @@
 package com.java.oauth2.controller;
 
+import com.java.oauth2.common.Utils;
+import com.java.oauth2.dto.FileDTO;
+import com.java.oauth2.dto.FileResDTO;
 import com.java.oauth2.dto.OauthReqDTO;
 import com.java.oauth2.dto.CustomOAuth2User;
+import com.java.oauth2.entity.FileInfo;
 import com.java.oauth2.entity.OAuthClient;
+import com.java.oauth2.repository.OAuthClientRepository;
+import com.java.oauth2.service.FileService;
 import com.java.oauth2.service.OAuthServiceImp;
 import com.java.oauth2.service.PostServiceImp;
 import com.nimbusds.jose.jwk.JWKSet;
@@ -16,6 +22,7 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("/")
 @Slf4j
@@ -32,7 +39,7 @@ public class OAuthClientController {
   public String home(@AuthenticationPrincipal CustomOAuth2User oAuth2User, Model model, HttpServletRequest request, HttpServletResponse response, @RequestHeader(value = "Authorization", defaultValue = "") String authorizationHeader) {
     return oAuthService.getLoginInfo(request, model);
   }
-  
+
   @ResponseBody
   @GetMapping("/userinfo")
   public OAuthClient userinfo (HttpServletRequest request) {
@@ -47,6 +54,16 @@ public class OAuthClientController {
   @GetMapping("/signUp")
   public String signUp() {
     return "signUp";
+  }
+
+  @GetMapping("/MyPageInfo")
+  public String MyPageInfo(HttpServletRequest request, Model model) {
+    return oAuthService.MyPageInfo(request,model);
+  }
+
+  @GetMapping("/MyPageEdit")
+  public String MyPageEdit(HttpServletRequest request, Model model) {
+    return oAuthService.MyPageEdit(request,model);
   }
 
   @ResponseBody
@@ -65,7 +82,17 @@ public class OAuthClientController {
   public String logout(Model model, HttpServletResponse response) {
     return oAuthService.logout(model,response);
   }
-  
 
+  @ResponseBody
+  @PostMapping("/UserInfoUpdate")
+  public boolean UserInfoUpdate(@RequestParam(value = "file", required = false) MultipartFile file,
+                                   @RequestParam("email") String email,
+                                   @RequestParam("name") String name,
+                                   @RequestParam("pwd") String pwd,
+                                   HttpServletRequest request) {
+    System.out.println("UserInfoUpdate start");
+    
+    return oAuthService.UserInfoUpdate(file,email,name,pwd,request);
+  }
 
 }
